02bafd9c423a9d31491650891a4342f9c7385774
NicBeltramelli <nicola.beltramelli@gmail.com>
2018-09-06 04:01:53 +0200

    Add the layout setting

---
+++ resources/lib/structure/layout.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Genesis Advanced
+ *
+ * This file adds the layout setting.
+ *
+ * @package Genesis Advanced
+ * @author  NicBeltramelli
+ * @license GPL-2.0+
+ * @link    https://github.com/NicBeltramelli/genesis-advanced.git
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Removes site layouts.
+genesis_unregister_layout( 'content-sidebar-sidebar' );
+genesis_unregister_layout( 'sidebar-content-sidebar' );
+genesis_unregister_layout( 'sidebar-sidebar-content' );
