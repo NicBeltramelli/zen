5d08cf6c96fe73dcf2ec8e294fc0db815379d7f1
NicBeltramelli <nicola.beltramelli@gmail.com>
2018-09-06 03:53:29 +0200

    Add navigation setting

---
+++ resources/lib/structure/navigation.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Genesis Advanced
+ *
+ * This file adds the navigation setting.
+ *
+ * @package Genesis Advanced
+ * @author  NicBeltramelli
+ * @license GPL-2.0+
+ * @link    https://github.com/NicBeltramelli/genesis-advanced.git
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Defines responsive menu settings
+ *
+ * @since 2.3.0
+ */
+function genesis_advanced_responsive_menu_settings() {
+
+	$settings =
+	[
+		'mainMenu'         => __( 'Menu', 'genesis-advanced' ),
+		'menuIconClass'    => 'dashicons-before dashicons-menu',
+		'subMenu'          => __( 'Submenu', 'genesis-advanced' ),
+		'subMenuIconClass' => 'dashicons-before dashicons-arrow-down-alt2',
+		'menuClasses'      =>
+		[
+			'combine' =>
+			[
+				'.nav-primary',
+			],
+			'others'  => [],
+		],
+	];
+
+	return $settings;
+
+}
+
+// Removes output of primary navigation right extras.
+remove_filter( 'genesis_nav_items', 'genesis_nav_right', 10, 2 );
+remove_filter( 'wp_nav_menu_items', 'genesis_nav_right', 10, 2 );
+
+// Repositions primary navigation menu.
+remove_action( 'genesis_after_header', 'genesis_do_nav' );
+add_action( 'genesis_header', 'genesis_do_nav', 12 );
+
+// Repositions the secondary navigation menu.
+remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+add_action( 'genesis_footer', 'genesis_do_subnav', 10 );
+
+/**
+ * Reduces secondary navigation menu to one level depth
+ *
+ * @since 2.2.3
+ *
+ * @param array $args Original menu options.
+ * @return array Menu options with depth set to 1.
+ */
+add_filter( 'wp_nav_menu_args', function ( $args ) {
+
+	if ( 'secondary' !== $args['theme_location'] ) {
+		return $args;
+	}
+
+	$args['depth'] = 1;
+	return $args;
+
+} );
