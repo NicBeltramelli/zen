34a39af14cfc3786933fad5da2e8bd9a83df1b62
NicBeltramelli <nicola.beltramelli@gmail.com>
2018-09-06 04:47:32 +0200

    Extras functions

---
+++ lib/extras.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Genesis Advanced
+ *
+ * This file adds your custom functions.
+ *
+ * @package Genesis Advanced
+ * @author  NicBeltramelli
+ * @license GPL-2.0+
+ * @link    https://github.com/NicBeltramelli/genesis-advanced.git
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
