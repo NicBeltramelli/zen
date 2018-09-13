622c4ee4525d50b7559599d49192609680b3bd32
NicBeltramelli <nicola.beltramelli@gmail.com>
2018-09-13 21:25:29 +0200

    Add front page template

---
+++ front-page.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Genesis Advanced
+ *
+ * This file adds the front page template.
+ *
+ * @package Genesis Advanced
+ * @author  NicBeltramelli
+ * @license GPL-2.0-or-later
+ * @link    https://github.com/NicBeltramelli/genesis-advanced.git
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+genesis();
+
