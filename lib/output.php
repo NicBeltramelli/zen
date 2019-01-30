bbfeb7030a29c75be44e54f8f42b44db7fbd8b48
NicBeltramelli <nicola.beltramelli@gmail.com>
2019-01-30 02:04:41 +0100

    Update inline styles

--- lib/output.php
+++ lib/output.php
@@ -44,105 +44,116 @@ add_action(
 		/* Output link color inline css */
 		$css .= ( genesis_advanced_customizer_get_default_link_color() !== $color_link ) ? sprintf(
 			'
-
-		a,
-		.entry-title a:focus,
-		.entry-title a:hover,
-		.genesis-nav-menu a:focus,
-		.genesis-nav-menu a:hover,
-		.genesis-nav-menu .menu-item.current-menu-item > a,
-		.menu-toggle:focus,
-		.menu-toggle:hover,
-		.sub-menu-toggle:focus,
-		.sub-menu-toggle:hover {
-			color: %s;
-		}
-
-		', $color_link
+			a {
+				color: %s;
+			}
+			', $color_link, genesis_advanced_color_contrast( $color_link )
 		) : '';
 
 		/* Output accent color inline css */
 		$css .= ( genesis_advanced_customizer_get_default_accent_color() !== $color_accent ) ? sprintf(
 			'
-		.button.accent,
-		a.button.accent,
-		button.accent,
-		input[type="button"].accent,
-		input[type="reset"].accent,
-		input[type="submit"].accent {
-			background-color: %1$s;
-		}
-
-		.button.outline.accent,
-		a.button.outline.accent,
-		button.outline.accent,
-		input[type="button"].outline.accent,
-		input[type="reset"].outline.accent,
-		input[type="submit"].outline.accent {
-			border-color: %1$s;
-			color: %1$s;
-		}
-
-		@media only screen and (min-width: 900px) {
-			.genesis-nav-menu > .menu-item.menu-highlight > a,
-			.genesis-nav-menu > .menu-item.menu-highlight.current-menu-item > a {
+			.button.accent,
+			a.button.accent,
+			button.accent,
+			input[type="button"].accent,
+			input[type="reset"].accent,
+			input[type="submit"].accent,
+			.has-accent-background-color {
 				background-color: %1$s;
 			}
-		}
-		', $color_accent, genesis_advanced_color_contrast( $color_accent )
+
+			.genesis-nav-menu a:focus,
+			.genesis-nav-menu a:hover,
+			.genesis-nav-menu .menu-item.current-menu-item > a,
+			.menu-toggle:focus,
+			.menu-toggle:hover,
+			.sub-menu-toggle:focus,
+			.sub-menu-toggle:hover,
+			.entry-title > a:focus,
+			.entry-title > a:hover,
+			.pagination-previous > a:focus,
+			.pagination-previous > a:hover,
+			.pagination-next > a:focus,
+			.pagination-next > a:hover,
+			.widget-title > a:focus,
+			.widget-title > a:hover,
+			.sidebar .widget ul li > a:focus,
+			.sidebar .widget ul li > a:hover,
+			.has-accent-color {
+				color: %1$s;
+			}
+
+			.button.outline.accent,
+			a.button.outline.accent,
+			button.outline.accent,
+			input[type="button"].outline.accent,
+			input[type="reset"].outline.accent,
+			input[type="submit"].outline.accent {
+				border-color: %1$s;
+				color: %1$s;
+			}
+
+			@media only screen and (min-width: 900px) {
+				.genesis-nav-menu > .menu-item.menu-highlight > a,
+				.genesis-nav-menu > .menu-item.menu-highlight.current-menu-item > a {
+					background-color: %1$s;
+				}
+			}
+			', $color_accent, genesis_advanced_color_contrast( $color_accent )
 		) : '';
 
 		/* Output custom logo inline css */
 		$css .= ( has_custom_logo() && ( 200 <= $logo_effective_height ) ) ?
-		'
-		.site-header {
-			position: static;
-		}
-		'
+			'
+			.site-header {
+				position: static;
+			}
+			'
 		: '';
 
 		/* Output custom logo inline css */
 		$css .= ( has_custom_logo() && ( 350 !== $logo_max_width ) ) ? sprintf(
 			'
-		.wp-custom-logo .site-container .title-area {
-			max-width: %spx;
-		}
-		', $logo_max_width
+			.wp-custom-logo .site-container .title-area {
+				max-width: %spx;
+			}
+			', $logo_max_width
 		) : '';
 
 		/* Place menu below logo and center logo once it gets big */
 		$css .= ( has_custom_logo() && ( 600 <= $logo_max_width ) ) ?
-		'
-		.wp-custom-logo .title-area,
-		.wp-custom-logo .menu-toggle,
-		.wp-custom-logo .nav-primary {
-			float: none;
-		}
-
-		.wp-custom-logo .title-area {
-			margin: 0 auto;
-			text-align: center;
-		}
-
-		@media only screen and (min-width: 900px) {
+			'
+			.wp-custom-logo .title-area,
+			.wp-custom-logo .menu-toggle,
 			.wp-custom-logo .nav-primary {
+				float: none;
+			}
+
+			.wp-custom-logo .title-area {
+				margin: 0 auto;
 				text-align: center;
 			}
 
-			.wp-custom-logo .nav-primary .sub-menu {
-				text-align: left;
+			@media only screen and (min-width: 900px) {
+				.wp-custom-logo .nav-primary {
+					text-align: center;
+				}
+
+				.wp-custom-logo .nav-primary .sub-menu {
+					text-align: left;
+				}
 			}
-		}
-		'
+			'
 		: '';
 
 		/* Output custom logo inline css */
 		$css .= ( has_custom_logo() && $logo_padding ) ? sprintf(
 			'
-		.wp-custom-logo .title-area {
-			padding-top: %spx;
-		}
-		', $logo_padding + 5
+			.wp-custom-logo .title-area {
+				padding-top: %spx;
+			}
+			', $logo_padding + 5
 		) : '';
 
 		if ( $css ) {
--- lib/woocommerce/woocommerce-output.php
+++ lib/woocommerce/woocommerce-output.php
@@ -36,50 +36,47 @@ add_action(
 
 		$woo_css .= ( genesis_advanced_customizer_get_default_link_color() !== $color_link ) ? sprintf(
 			'
-		.woocommerce .woocommerce-breadcrumb a:hover,
-		.woocommerce .woocommerce-breadcrumb a:focus,
-		.woocommerce .widget_layered_nav ul li.chosen a::before,
-		.woocommerce .widget_layered_nav_filters ul li a::before,
-		.woocommerce .widget_rating_filter ul li.chosen a::before {
-			color: %s;
-		}
-
-	', $color_link
+			.woocommerce .woocommerce-breadcrumb a:hover,
+			.woocommerce .woocommerce-breadcrumb a:focus,
+			.woocommerce .widget_layered_nav ul li.chosen a::before,
+			.woocommerce .widget_layered_nav_filters ul li a::before,
+			.woocommerce .widget_rating_filter ul li.chosen a::before {
+				color: %s;
+			}
+			', $color_link, genesis_advanced_color_contrast( $color_link )
 		) : '';
 
 		$woo_css .= ( genesis_advanced_customizer_get_default_accent_color() !== $color_accent ) ? sprintf(
 			'
-		.woocommerce a.button.alt,
-		.woocommerce a.button.alt,
-		.woocommerce button.button.alt,
-		.woocommerce button.button.alt,
-		.woocommerce input.button.alt,
-		.woocommerce input.button.alt,
-		.woocommerce a.button.alt:hover,
-		.woocommerce a.button.alt:focus,
-		.woocommerce button.button.alt:hover,
-		.woocommerce button.button.alt:focus,
-		.woocommerce input.button.alt:hover,
-		.woocommerce input.button.alt:focus,
-		.woocommerce span.onsale,
-		.woocommerce.widget_price_filter .ui-slider .ui-slider-handle,
-		.woocommerce.widget_price_filter .ui-slider .ui-slider-range {
-			background-color: %1$s;
-			color: %2$s;
-		}
+			.woocommerce a.button.alt,
+			.woocommerce a.button.alt,
+			.woocommerce button.button.alt,
+			.woocommerce button.button.alt,
+			.woocommerce input.button.alt,
+			.woocommerce input.button.alt,
+			.woocommerce a.button.alt:hover,
+			.woocommerce a.button.alt:focus,
+			.woocommerce button.button.alt:hover,
+			.woocommerce button.button.alt:focus,
+			.woocommerce input.button.alt:hover,
+			.woocommerce input.button.alt:focus,
+			.woocommerce span.onsale,
+			.woocommerce.widget_price_filter .ui-slider .ui-slider-handle,
+			.woocommerce.widget_price_filter .ui-slider .ui-slider-range {
+				background-color: %1$s;
+			}
 
-		.woocommerce-message {
-			border-top-color: %1$s;
-		}
+			.woocommerce-message {
+				border-top-color: %1$s;
+			}
 
-		.woocommerce div.product .woocommerce-tabs ul.tabs li a:hover,
-		.woocommerce div.product .woocommerce-tabs ul.tabs li a:focus,
-		.woocommerce div.product .woocommerce-tabs ul.tabs li.active a,
-		.woocommerce-message::before {
-			color: %1$s;
-		}
-
-	', $color_accent, genesis_advanced_color_contrast( $color_accent )
+			.woocommerce div.product .woocommerce-tabs ul.tabs li a:hover,
+			.woocommerce div.product .woocommerce-tabs ul.tabs li a:focus,
+			.woocommerce div.product .woocommerce-tabs ul.tabs li.active a,
+			.woocommerce-message::before {
+				color: %1$s;
+			}
+			', $color_accent, genesis_advanced_color_contrast( $color_accent )
 		) : '';
 
 		if ( $woo_css ) {
