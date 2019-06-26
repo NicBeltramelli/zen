3966c89658b0cb14d084dc789338f82d2bd57c00
NicBeltramelli <nicola.beltramelli@gmail.com>
2019-06-26 21:03:11 +0200

    Improve style

--- assets/scripts/routes/common.js
+++ assets/scripts/routes/common.js
@@ -4,18 +4,5 @@ export default {
 	},
 	finalize() {
 		// JavaScript to be fired on all pages, after page specific JS is fired
-
-		// Prevent body from scrolling when responsive menu is activated
-		if ($(window).width() < 900){
-
-			$( ".menu-toggle" ).click(function(){
-
-				if ($( "body" ).hasClass("no-scroll")) {
-					$( "body" ).removeClass( "no-scroll" );
-				} else {
-					$( "body" ).addClass( "no-scroll" );
-				}
-			})
-		}
 	},
 };
