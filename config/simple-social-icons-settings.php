fcc327eff60916f09bdbd70381fc4e232ddb019d
NicBeltramelli <nicola.beltramelli@gmail.com>
2019-08-18 04:57:03 +0200

    Add simple social icons default settings through the configuration api

---
+++ config/simple-social-icons-settings.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Genesis Advanced
+ *
+ * This file adds the Simple Social Icons default settings
+ *
+ * @package Genesis Advanced
+ * @author  NicBeltramelli
+ * @license GPL-2.0-or-later
+ * @link    https://github.com/NicBeltramelli/genesis-advanced.git
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+return [
+	'alignment'              => 'alignleft',
+	'background_color'       => '#f5f5f5',
+	'background_color_hover' => '#333333',
+	'border_radius'          => 3,
+	'border_width'           => 0,
+	'icon_color'             => '#333333',
+	'icon_color_hover'       => '#ffffff',
+	'size'                   => 40,
+];
--- lib/defaults.php
+++ lib/defaults.php
@@ -26,20 +26,8 @@ add_filter(
 	'simple_social_default_styles',
 	function ( $defaults ) {
 
-		$args = [
-			'alignment'              => 'alignleft',
-			'background_color'       => '#f5f5f5',
-			'background_color_hover' => '#333333',
-			'border_radius'          => 3,
-			'border_width'           => 0,
-			'icon_color'             => '#333333',
-			'icon_color_hover'       => '#ffffff',
-			'size'                   => 40,
-		];
+		$args = genesis_get_config( 'simple-social-icons-settings' );
 
-		$args = wp_parse_args( $args, $defaults );
-
-		return $args;
-
+		return wp_parse_args( $args, $defaults );
 	}
 );
